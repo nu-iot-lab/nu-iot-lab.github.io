042a049b39db17492833f03563dd0209339f7293
ltwmori <ltwmori@gmail.com>
2022-12-09 02:57:03 +0600

    feat: members

---
+++ src/data/MembersData.ts
@@ -0,0 +1,47 @@
+export const MembersData = [
+  {
+    fullName: "Dimitrios Zorbas",
+    position: "Professor",
+    email: "dimitrios.zorbas@nu.edu.kz",
+    description:
+      "School of Engineering and Digital Sciences, Department of Computer Science",
+    image:
+      "https://ieeexplore.ieee.org/mediastore_new/IEEE/content/freeimages/6287639/9312710/9420069/zorba-3076861-small.gif",
+  },
+  {
+    fullName: "Dimitrios Zorbas",
+    position: "Professor",
+    email: "dimitrios.zorbas@nu.edu.kz",
+    description:
+      "School of Engineering and Digital Sciences, Department of Computer Science",
+    image:
+      "https://ieeexplore.ieee.org/mediastore_new/IEEE/content/freeimages/6287639/9312710/9420069/zorba-3076861-small.gif",
+  },
+  {
+    fullName: "Dimitrios Zorbas",
+    position: "Professor",
+    email: "dimitrios.zorbas@nu.edu.kz",
+    description:
+      "School of Engineering and Digital Sciences, Department of Computer Science",
+    image:
+      "https://ieeexplore.ieee.org/mediastore_new/IEEE/content/freeimages/6287639/9312710/9420069/zorba-3076861-small.gif",
+  },
+  {
+    fullName: "Dimitrios Zorbas",
+    position: "Professor",
+    email: "dimitrios.zorbas@nu.edu.kz",
+    description:
+      "School of Engineering and Digital Sciences, Department of Computer Science",
+    image:
+      "https://ieeexplore.ieee.org/mediastore_new/IEEE/content/freeimages/6287639/9312710/9420069/zorba-3076861-small.gif",
+  },
+  {
+    fullName: "Dimitrios Zorbas",
+    position: "Professor",
+    email: "dimitrios.zorbas@nu.edu.kz",
+    description:
+      "School of Engineering and Digital Sciences, Department of Computer Science",
+    image:
+      "https://ieeexplore.ieee.org/mediastore_new/IEEE/content/freeimages/6287639/9312710/9420069/zorba-3076861-small.gif",
+  },
+];
--- src/pages/Members.tsx
+++ src/pages/Members.tsx
@@ -1,41 +1,64 @@
-import React from 'react'
+import {
+  Paper,
+  Avatar,
+  Text,
+  Container,
+  createStyles,
+  Grid,
+} from "@mantine/core";
+import React from "react";
+import { MembersData } from "../data/MembersData";
+
+const useStyles = createStyles((theme) => ({
+  card: {},
+  gridCard: {
+    "&:last-child:nth-child(3n-1)": {
+      display: "flex",
+      justifyContent: "center",
+    },
+    "&:nth-last-child(2):nth-child(3n+1)": {
+      display: "flex",
+      justifyContent: "center",
+    },
+  },
+}));
 
 const Members = () => {
+  const { classes } = useStyles();
   return (
-    // <div>
-    //   <div class="post-card"><a class="post-card__category" href="post.html">category</a>
-    //         <div class="post-card__featured-image"></div>
-    //         <div class="post-card__details">
-    //           <h2><a href="post.html">Некоей равно порицающих или превратное действительно несло пользы возникает предпочел презирает нет некоей то истину это.            
-    //               </a></h2>
-    //           <p>Обстоятельства eсли такие которое или которого великие истину потому восхваляющих. Превратное никаких как разумно, нет раз говорил если из-за отвергает - или нет которого, какими. Обстоятельства иной потому раскрою мог человек лишь предаваться наслаждению неприятностей равно откуда избегал порицающих - ни умеет, презирает немалое равно действительно простейшим или. Несло, назвал обстоятельства предпочел которого с это презирает поняли страдание ни предпочел умеет, если вы постигают чтобы, стал тех картину стремящегося. Этот разъясню которого стремящегося равно, представление бы ни стал некоей справедливости умеет что - немалое.
-    //           </p><a class="post-card__avatar" href="post.html">
-    //             <div class="avatar__circle"></div>
-    //             <div class="avatar__name">by Ded pihto</div>
-    //             <div class="avatar__muted-line"></div></a>
-    //         </div>
-    //       </div>
-    // </div>
-    <div>
-        <h2>Dimitrios Zorbas</h2>
-        <img src="https://ieeexplore.ieee.org/mediastore_new/IEEE/content/freeimages/6287639/9312710/9420069/zorba-3076861-small.gif" alt="" />
-        <div>
-            <p>Assistant professor</p>
-            <p>School of Engineering and Digital Sciences, Department of Computer Science</p>
-            <p><a href = "https://orcid.org/0000-0001-6682-9071">https://orcid.org/0000-0001-6682-9071</a></p>
-        </div>
-        <div>
-           <h3>Publications Showcase</h3>
-           <ol>
-                <li><a href = "https://www.sciencedirect.com/science/article/pii/S0140366422003401">D. Zorbas, "Improving LoRaWAN Downlink Performance in the EU868 Spectrum", Computer Communications, Vol. 195, Nov. 2022, pp. 303-314</a></li>
-                <li><a href = "https://www.sciencedirect.com/science/article/pii/S0140366422003401">D. Zorbas, "Improving LoRaWAN Downlink Performance in the EU868 Spectrum", Computer Communications, Vol. 195, Nov. 2022, pp. 303-314</a></li>
-                <li><a href = "https://www.sciencedirect.com/science/article/pii/S0140366422003401">D. Zorbas, "Improving LoRaWAN Downlink Performance in the EU868 Spectrum", Computer Communications, Vol. 195, Nov. 2022, pp. 303-314</a></li>
-           </ol>
-        </div>
+    <Container>
+      <Grid gutter={20}>
+        {MembersData.map((member) => (
+          <Grid.Col span={4} className={classes.gridCard}>
+            <Paper
+              radius="md"
+              withBorder
+              p="lg"
+              className={classes.card}
+              sx={(theme) => ({
+                backgroundColor:
+                  theme.colorScheme === "dark"
+                    ? theme.colors.dark[8]
+                    : theme.white,
+              })}
+            >
+              <Avatar src={member.image} size={120} radius={120} mx="auto" />
+              <Text align="center" size="lg" weight={500} mt="md">
+                {member.fullName}
+              </Text>
+              <Text align="center" color="dimmed" size="sm">
+                {member.email} • {member.position}
+              </Text>
 
-    </div>
-    
-  )
-}
+              <Text align="center" color="dimmed" size="sm">
+                {member.description}
+              </Text>
+            </Paper>
+          </Grid.Col>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
 
-export default Members
+export default Members;
