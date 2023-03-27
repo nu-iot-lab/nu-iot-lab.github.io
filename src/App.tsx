eb7446dfb103ad95b10e8f19ccdc8c71f6cac722
ltwmori <ltwmori@gmail.com>
2023-03-27 17:06:26 +0600

    fix: app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { FooterSimple } from "./components/Footer";
 import { HeaderResponsive } from "./components/Header";
 import { ROUTES } from "./consts/routes";
 
-//
 function App() {
   return (
     <div className="App">
